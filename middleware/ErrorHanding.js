36b168e3f80e1aa3747a6c52af1d2398957d7bbf
Tarynleewalbrugh25 <walbrught@gmail.com>
2024-02-15 11:36:58 +0200

    done

---
+++ middleware/ErrorHanding.js
@@ -0,0 +1,14 @@
+function errorHandling(err, req, res, next){
+    if(err || res.statusCode >= 400 ){
+        res.json({
+            status: err.status || res.
+            statusCode || 500,
+            msg: 'An error try again later.'
+        })
+    }else {
+        next()
+    }
+}
+export {
+    errorHandling
+}
